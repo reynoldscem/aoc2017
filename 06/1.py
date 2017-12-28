f91456eaf8fc2bbe63256caec41a44fa64c011bd
Charles Reynolds <reynoldscem@gmail.com>
2017-12-28 17:53:45 +0000

    Add some comments

--- 06/1.py
+++ 06/1.py
@@ -13,6 +13,10 @@ def build_parser():
 
 
 def get_index_iterator(index_of_bank, num_banks, num_blocks):
+    # Get an iterator for the next bank until the end of list index,
+    #  then get a cycling iterator for all banks. Join them,
+    #  and take the correct number of its before throwing a
+    #  StopIteration.
     first_cycle = range(index_of_bank + 1, num_banks)
     index_cycles = cycle(range(num_banks))
 
@@ -23,14 +27,24 @@ def get_index_iterator(index_of_bank, num_banks, num_blocks):
 
 def main():
     args = build_parser().parse_args()
+
+    # Just assume filename is legit, and format
+    #  is correct.
     with open(args.filename) as fd:
         data = fd.read().strip().split()
 
+    # Need to modify values / use .index.
+    # So construct a list.
     banks = list(map(int, data))
 
+    # To track if we're in a cycle.
     states = set()
 
     for cycle_index in count(1):
+        # Luckily ties are broken by the lowest index,
+        #  which matches python's behaviour.
+        # Grab the bank with highest value, record that value,
+        #  and zero that bank out.
         max_value = max(banks)
         max_index = banks.index(max_value)
         banks[max_index] = 0
@@ -41,10 +55,15 @@ def main():
                 ''.format(max_index + 1, max_value)
             )
 
+        # Given the current info get an iterator for where
+        #  to deposit blocks. Walk over it increment.
         index_iterator = get_index_iterator(max_index, len(banks), max_value)
         for distribution_index in index_iterator:
             banks[distribution_index] += 1
 
+        # Make state hashable by constructing a tuple.
+        #  Check if it's in our visited set, if it is break,
+        #  if not add it in.
         frozen_state = tuple(banks)
         if frozen_state in states:
             break
