f128440d070e5e69750024b845b71ff655fbd79b
Charles EM Reynolds <reynoldscem@gmail.com>
2018-01-08 08:25:33 +0000

    Start day 7

--- 07/1.py
+++ 07/1.py
@@ -1,6 +1,12 @@
 from argparse import ArgumentParser
-import operator
-import re
+import copy
+
+
+class Program():
+    def __init__(self, name, weight, holding):
+        self.name = name
+        self.weight = weight
+        self.holding = copy.deepcopy(holding)
 
 
 def build_parser():
@@ -11,35 +17,11 @@ def build_parser():
     return parser
 
 
-def parse_instruction(raw_instruction):
-    instruction_regex = re.compile(
-        '^([a-z]+) (inc|dec) (-?\d+) if ([a-z]+) (!=|>|>=|<|<=|==) (-?\d+)$'
-    )
-    token_LUT = {
-        'inc': operator.
-    }
-    (
-        register_name, operator_token, operand, compar_1, comparator, compar_2
-    ) = re.match(instruction_regex).groups()
-
-    operand = int(operand)
-    compar_2 = int(compar_2)
-
-    operator = token_LUT[operator_token]
-
-
 def main():
     args = build_parser().parse_args()
     with open(args.filename) as fd:
-        data = fd.read().strip()
-
-    accum = 0
-    data_wrapped = data + data[0]
-    for index in range(len(data_wrapped) - 1):
-        first_char, second_char = data_wrapped[index:index + 2]
-        if first_char == second_char:
-            accum += int(first_char)
-    print(accum)
+        data = fd.read().splitlines()
+    data
 
 
 if __name__ == '__main__':
