0ae57f53ce6882fe5af1b0954f10ac7f5246d8b3
Charles EM Reynolds <reynoldscem@gmail.com>
2018-07-29 11:03:55 +0100

    Day 7 part 1

--- 07/1.py
+++ 07/1.py
@@ -7,6 +7,11 @@ class Program():
         self.name = name
         self.weight = weight
         self.holding = copy.deepcopy(holding)
+        self.is_carrying = len(self.holding) >= 1
+
+    def __str__(self):
+        holding_str = ', '.join(self.holding)
+        print('{} ({}) -> {}'.format(self.name, self.weight, holding_str))
 
 
 def build_parser():
@@ -17,11 +22,31 @@ def build_parser():
     return parser
 
 
+def parse_entry(line):
+    import re
+
+    line = re.sub('[()\->,]', '', line)
+    element_id, weight_string, *rest = line.split()
+    weight = int(weight_string)
+
+    return Program(element_id, weight, rest)
 def main():
     args = build_parser().parse_args()
     with open(args.filename) as fd:
         data = fd.read().splitlines()
-    data
+
+    entries = [parse_entry(line) for line in data]
+    candidates = {}
+    blacklist = set()
+
+    for entry in entries:
+        for held_disk_id in entry.holding:
+            blacklist.add(held_disk_id)
+            candidates.pop(held_disk_id, None)
+        if entry.is_carrying and entry.name not in blacklist:
+            candidates[entry.name] = entry
+
+    print(list(candidates)[0])
 
 
 if __name__ == '__main__':
